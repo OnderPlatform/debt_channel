1edbbe8295b9850f63b64a7f639e77a8ef77a7a5
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-12-09 02:55:55 +0800

    Basic add debt

--- packages/contracts/contracts/Holding.sol
+++ packages/contracts/contracts/Holding.sol
@@ -4,18 +4,35 @@ import "./vendor/IERC20.sol";
 import "./vendor/SafeMath.sol";
 import "./vendor/Ownable.sol";
 import "./vendor/SignerRole.sol";
+import "./vendor/ECDSA.sol";
 
 contract Holding is Ownable, SignerRole {
     using SafeMath for uint256;
 
     mapping (address => uint256) public deposits;
+    mapping (address => mapping (address => uint256)) public debts;
 
     event DidDeposit(address indexed token, uint256 amount);
+    event DidAddDebt(address indexed destination, address indexed token, uint256 amount);
 
     function deposit (address _token, uint256 _amount) public {
         IERC20 token = IERC20(_token);
         require(token.transferFrom(msg.sender, address(this), _amount), "Unable to transfer token to the contract");
         deposits[_token] = deposits[_token].add(_amount);
         emit DidDeposit(_token, _amount);
+    }
+
+    function addDebt (address _destination, address _token, uint256 _amount, bytes memory _sigMine, bytes memory _sigOther) public {
+        debts[_destination][_token] = debts[_destination][_token].add(_amount);
+        bytes32 digest = ECDSA.toEthSignedMessageHash(addDebtDigest(_destination, _token, _amount));
+        address recovered = ECDSA.recover(digest, _sigMine);
+        require(isSigner(recovered), "Should be signed by me");
+        // check if sigMe is ok
+        // check if sigOther is ok
+        emit DidAddDebt(_destination, _token, _amount);
+    }
+
+    function addDebtDigest (address _destination, address _token, uint256 _amount) public pure returns (bytes32) {
+        return keccak256(abi.encode('ad', _destination, _token, _amount));
     }
 }
--- packages/contracts/src/Holding.test.ts
+++ packages/contracts/src/Holding.test.ts
@@ -3,6 +3,7 @@ import * as chai from 'chai'
 import * as BigNumber from 'bignumber.js'
 import * as asPromised from 'chai-as-promised'
 import * as contracts from './'
+import * as util from 'ethereumjs-util'
 import * as sigUtil from 'eth-sig-util'
 import TestToken from './wrappers/TestToken'
 
@@ -17,34 +18,66 @@ const Token = artifacts.require<TestToken.Contract>('support/TestToken.sol')
 const WRONG_CHANNEL_ID = '0xdeadbeaf'
 const WRONG_SIGNATURE = '0xcafebabe'
 
-contract('Holding', accounts => {
-  const sender = accounts[0]
+async function signature (signer: string, digestHex: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const checksummed = util.toChecksumAddress(signer)
+    web3.eth.sign(digestHex, checksummed, (err, signature) => {
+      err ? reject(err) : resolve(signature)
+    })
+  })
+}
 
-  let instance: contracts.Holding.Contract
+contract('Holding', accounts => {
+  const ALICE = accounts[0]
+  const BOB = accounts[1]
+
+  let instanceA: contracts.Holding.Contract
+  let instanceB: contracts.Holding.Contract
   let token: TestToken.Contract
 
   beforeEach(async () => {
     token = await Token.new()
-    await token.mint(sender, 1000)
-    instance = await Holding.new()
+    await token.mint(ALICE, 1000)
+    await token.mint(BOB, 1000)
+    instanceA = await Holding.new()
+    instanceB = await Holding.new()
   })
 
-  describe('constructor', () => {
-    specify('set owner', async () => {
-      const holding = await Holding.new({from: sender})
-      const owner = await holding.owner()
-      assert.equal(owner, sender)
+  specify('constructor', async () => {
+    const holding = await Holding.new({from: ALICE})
+    const owner = await holding.owner()
+    assert.equal(owner, ALICE)
+  })
+
+  specify('.deposit', async () => {
+    await token.approve(instanceA.address, 1000, {from: ALICE})
+    await instanceA.deposit(token.address, 100, {from: ALICE})
+    const tokenBalance = await token.balanceOf(instanceA.address)
+    assert.equal(tokenBalance.toString(), '100')
+    const holdingBalance = await instanceA.deposits(token.address)
+    assert.equal(holdingBalance.toString(), '100')
+  })
+
+  describe('.addDebt', () => {
+    specify('add debt', async () => {
+      const amount = 10
+      const digest = await instanceA.addDebtDigest(instanceB.address, token.address, amount)
+      const signatureA = await signature(ALICE, digest)
+      const tx = await instanceA.addDebt(instanceB.address, token.address, amount, signatureA, signatureA)
+      const event = tx.logs[0]
+      assert(contracts.Holding.isDidAddDebtEvent(event))
+      const debt = await instanceA.debts(instanceB.address, token.address)
+      assert.equal(debt.toString(), '10')
     })
-  })
 
-  describe('.deposit', () => {
-    specify('emit DidOpen event', async () => {
-      await token.approve(instance.address, 1000, {from: sender})
-      await instance.deposit(token.address, 100, {from: sender})
-      const tokenBalance = await token.balanceOf(instance.address)
-      assert.equal(tokenBalance.toString(), '100')
-      const holdingBalance = await instance.deposits(token.address)
-      assert(holdingBalance.toString(), '100')
+    specify('pass if sigMine is incorrect', async () => {
+    })
+
+    specify('ok if signed by delegate key', async () => {
+    })
+
+    specify('pass if sigOther is incorrect', async () => {
+
     })
   })
 })
